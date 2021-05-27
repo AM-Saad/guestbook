154a345e5f891c5b86b51fbffa7e8a31c269a4ec
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-27 23:16:11 +0200

    Saving new user to DB

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@ const http = require('http');
 const url = require('url');
 
 const User = require("./models/User").User;
+
 module.exports = http.createServer((req, res) => {
 
     const reqUrl = url.parse(req.url, true);
@@ -15,41 +16,49 @@ module.exports = http.createServer((req, res) => {
         })
         req.on('end', function () {
             console.log(`Compelete Data  ${body}`)
+            const newUser = new User(
+                name,
+                email,
+                password,
+            );
+            newUser.save();
         })
-        
-
+    
     }
+
+
+}
 
     // Login Endpoint
     if (reqUrl.pathname == '/login' && req.method === 'POST') {
 
-    }
+}
 
-    // Messages Endpoint
-    if (reqUrl.pathname == '/messages' && req.method === 'GET') {
+// Messages Endpoint
+if (reqUrl.pathname == '/messages' && req.method === 'GET') {
 
-    }
-
-
-    // New Message Endpoint
-    if (reqUrl.pathname == '/messages' && req.method === 'POST') {
-
-    }
+}
 
 
-    // Edit Message Endpoint
-    if (reqUrl.pathname == '/messages' && req.method === 'PUT') {
+// New Message Endpoint
+if (reqUrl.pathname == '/messages' && req.method === 'POST') {
 
-    }
+}
 
-    // Delete Message Endpoint
-    if (reqUrl.pathname == '/messages' && req.method === 'DELETE') {
 
-    }
+// Edit Message Endpoint
+if (reqUrl.pathname == '/messages' && req.method === 'PUT') {
 
-    // Reply To Message Endpoint
-    if (reqUrl.pathname == '/messages' && req.method === 'PUT') {
+}
 
-    }
+// Delete Message Endpoint
+if (reqUrl.pathname == '/messages' && req.method === 'DELETE') {
+
+}
+
+// Reply To Message Endpoint
+if (reqUrl.pathname == '/messages' && req.method === 'PUT') {
+
+}
 
 });
