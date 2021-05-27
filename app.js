1ffc0eb14e6808c029bb8c045de52afde5bdd11f
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-27 21:48:03 +0200

    Connected to database successfully

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const http = require('http');
+const mongoConnect = require("./util/db").mongoConnect;
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
 
 const server = http.createServer((req, res) => {
 
@@ -10,3 +13,5 @@ const port =  3000
 server.listen(port, () => {
     console.log(`Server running at ${port}`);
 });
+
+mongoConnect(() => {});
